48e7eeb2d58d0cafdbaddad96938a2b0fe85fc0d
romeur698-a11y <romeur698@gmail.com>
2025-11-21 23:09:31 -0300

    Update HomeContainer.tsx

--- components/HomeContainer.tsx
+++ components/HomeContainer.tsx
@@ -1,9 +1,9 @@
 // components/HomeContainer.tsx
 import React from 'react';
 
-export const HomeContainer =  { children }  : { children: React.ReactNode } => {
-  // O conteúdo original do seu HomeContainer deve ir aqui
-  return <div className="max-w-7xl mx-auto p-4">{children}</div>;
+export const HomeContainer =  children  : { children: React.ReactNode } => {
+  // O conteúdo original do seu HomeContainer deve vir aqui
+  return (
+    <div className='max-w-7xl mx-auto p-4'>{children}</div>
+  );
 };
-
-
