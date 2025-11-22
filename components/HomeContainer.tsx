96f3a04f9a555e6e496c9398ffbba5d71382f005
romeur698-a11y <romeur698@gmail.com>
2025-11-22 00:10:46 -0300

    Update HomeContainer.tsx

--- components/HomeContainer.tsx
+++ components/HomeContainer.tsx
@@ -1,9 +1,9 @@
 // components/HomeContainer.tsx
 import React from 'react';
 
-export const HomeContainer = ( { children } ) : { children: React.ReactNode } => {
-  // O conteúdo original do seu HomeContainer deve vir aqui
-  return (
+// Tipagem React.FC com sintaxe de função de seta correta
+export const HomeContainer: React.FC =   ( { children } )  => {
+  return 
     <div className='max-w-7xl mx-auto p-4'>{children}</div>
   );
 };
